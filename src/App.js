bab2e3349c88940e6a41107567c5cb5d3a51ce08
anilmohabiya <anilmohabiya1053@gmail.com>
2020-05-09 19:11:22 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,11 +3,12 @@ import './App.css';
 function App(){
     
     const [joke,setJoke]=useState("loading")
-    
-  const newJoke=()=>{
+    const [fName,setfN]=useState("")
+    const[sName,setsN]=useState("")
+  const newJoke=(first,second)=>{
 
          
-        fetch("http://api.icndb.com/").
+        fetch(`http://api.icndb.com/jokes/random?firstName=${first}&lastName=${second}`).
         then(resp=>resp.json()).then(result=>{
             console.log(result)
             setJoke(result.value.joke)
@@ -15,16 +16,17 @@ function App(){
       
   }
     useEffect(()=>{
-        newJoke()
+        newJoke(fName,sName)
     
     },[])
         return(
       <div>
             <h1>DEVLOPED BY-ANIL</h1>
-            <p>hits new to see new joke</p>
+            <p>Enter your name and read jokes on you</p>
             <h4>{joke}</h4>
-           
-            <button onClick={()=>newJoke()}>newJoke</button>
+            <input type="text" value={fName} onChange={(event)=>setfN(event.target.value)} placeholder="enter your Name" /><br/><br/>
+            <input type="text" value={sName} onChange={(event)=>setsN(event.target.value)} placeholder="enter your lastName"/><br/><br/>
+            <button onClick={()=>newJoke(fName,sName)}>newJoke</button>
             </div>
   
         );
